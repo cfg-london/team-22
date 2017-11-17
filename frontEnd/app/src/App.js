ca0254d404dbd2ce1f1a63e0e38be77a604363d3
dylanOshima <2017.dylan.oshima@isak.jp>
2017-11-18 00:21:24 +0100

    Adds Profile component. 'Learn more' function not working.

--- frontEnd/app/src/App.js
+++ frontEnd/app/src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 //Components
 import Profile from './components/profile';
+import Friends from './components/friends';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={require('./resources/logo-nobelprize_org.png')} className="App-logo" alt="logo" />
         </header>
-        <p className="App-intro">
+        <div className="App-intro">
           <Profile />
-        </p>
+
+        </div>
       </div>
     );
   }
--- frontEnd/app/src/components/profile.js
+++ frontEnd/app/src/components/profile.js
@@ -1,10 +1,49 @@
 import React, {Component} from 'react';
+import './profile.css';
 
 class Profile extends Component {
+  constructor(){
+    super();
+    this.state = {
+      image: require('../resources/testImage.jpg'),
+      body: "Martin Luther King Jr. - (January 15, 1929-April 4, 1968) was born Michael Luther King, Jr., but later had his name changed to Martin. His grandfather began the family's long tenure as pastors of the Ebenezer Baptist Church in Atlanta, serving from 1914 to 1931; his father has served from then until the present, and from 1960 until his death Martin Luther acted as co-pastor. Martin Luther attended segregated public schools in Georgia, graduating from high school at the age of fifteen; he received the B. A. degree in 1948 from Morehouse College, a distinguished Negro institution of Atlanta from which both his father and grandfather had graduated. After three years of theological study at Crozer Theological Seminary in Pennsylvania where he was elected president of a predominantly white senior class, he was awarded the B.D. in 1951. With a fellowship won at Crozer, he enrolled in graduate studies at Boston University, completing his residence for the doctorate in 1953 and receiving the degree in 1955. In Boston he met and married Coretta Scott, a young woman of uncommon intellectual and artistic attainments. Two sons and two daughters were born into the family.",
+      wantMore: false
+    }
+  }
+
+  // TODO: FIX THIS SHITE!
+  renderText(s){
+    if(s.length > 380){
+      console.log("LONG ASS STRING");
+      if(this.state.wantMore){
+        return s;
+      } else {
+        let output = s.slice(0,300)
+        return (
+          <p> {output} <a onClick={this.changeFlag()}>Learn more</a> </p>
+        )
+      }
+    } else {
+      console.log("SHORT ASS STRING");
+      return (<p> {s} </p>);
+    }
+  }
+
+  changeFlag(){
+    if(this.wantMore){
+      this.setState({wantMore: false})
+    } else {
+      this.setState({wantMore: true});
+    }
+  }
+
   render(){
     return(
       <div className="profile">
-        <img src={require('../resources/testImage.jpg')} alt="Dylan is sexy" />
+        <img src={this.state.image} className="profilePic" alt="Dylan is sexy" />
+        <div className="motivation">
+          {this.renderText(this.state.body)}
+        </div>
       </div>
     )
   }
