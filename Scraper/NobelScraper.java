80e98ca68c3540b48a614b3eaa6ec31739b26c0d
Rafael Irgolic <rafael.irgolic@gmail.com>
2017-11-17 21:59:51 +0000

    webscraper continued

--- Scraper/NobelScraper.java
+++ Scraper/NobelScraper.java
@@ -6,8 +6,46 @@ import java.net.URL;
 public class NobelScraper {
 
     public static void main (String[] args) throws Exception {
+        NobelScraper scraper = new NobelScraper();
+        scraper.scrape("peace", 1964, "king");
+    }
+
+    private void scrape(String subject, int year, String lastName) throws Exception {
+        final String[] sections = {
+                "bio",
+                "lecture",
+                "facts"
+        };
         URL url;
-        url = new URL("https://www.nobelprize.org/nobel_prizes/peace/laureates/1964/king-lecture.html");
+
+        for (String section : sections) {
+            url = new URL("https://www.nobelprize.org/nobel_prizes/" + subject + "/laureates/" + year + "/" + lastName + "-" + section + ".html");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.equals("<!-- Start Main Content -->")) {
+                    break;
+                }
+            }
+            PrintWriter writer = new PrintWriter(lastName + "-" + section + ".html");
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith("  <h2>")) {
+                    writer.println(line);
+                    break;
+                }
+            }
+            while ((line = reader.readLine()) != null) {
+                if (!line.equals("  <!--eri-no-index-->")) {
+                    writer.println(line);
+                } else {
+                    break;
+                }
+            }
+            writer.close();
+        }
+    }
+
+    private void scrape(URL url) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
         String line;
         while ((line = reader.readLine()) != null) {
@@ -17,8 +55,14 @@ public class NobelScraper {
         }
         PrintWriter writer = new PrintWriter("testScrape.html");
         while ((line = reader.readLine()) != null) {
-            if (!line.equals("<!--eri-no-index-->")) {
-                writer.write(line);
+            if (line.startsWith("  <h2>")) {
+                writer.println(line);
+                break;
+            }
+        }
+        while ((line = reader.readLine()) != null) {
+            if (!line.equals("  <!--eri-no-index-->")) {
+                writer.println(line);
             } else {
                 break;
             }
