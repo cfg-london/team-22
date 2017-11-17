e6c8541614a16277cb8e677a4684a5f5c41c57aa
dylanOshima <2017.dylan.oshima@isak.jp>
2017-11-17 22:44:24 +0100

    practice build

---
+++ frontEnd/README.md
@@ -0,0 +1,10 @@
+## Front End
+
+# Directory
+* App contains the barebones version of a react-app.
+* Will divide the different pages into the test site.
+
+# Running
+1. `cd /app`
+2. `npm install`
+3. `npm start`
--- frontEnd/app/src/App.js
+++ frontEnd/app/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//Components
+import Profile from './components/profile';
+
 class App extends Component {
   render() {
     return (
@@ -11,7 +14,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Profile />
         </p>
       </div>
     );
