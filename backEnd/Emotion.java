bd992988d0be694222e3fe037e0ea1359cbd35e4
Zenya <jevgenijchom@gmail.com>
2017-11-18 08:18:54 +0000

    Bios analysed and emotions of them extracted. File output.json has all the results.

---
+++ backEnd/Emotion.java
@@ -0,0 +1,14 @@
+
+public class Emotion {
+
+    public double anger,disgust,fear,joy,sadness;
+
+    public Emotion(double anger, double disgust, double fear, double joy, double sadness){
+        this.anger = anger;
+        this.disgust = disgust;
+        this.fear = fear;
+        this.joy = joy;
+        this.sadness = sadness;
+    }
+
+}
