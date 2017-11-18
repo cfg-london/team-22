32f93d44f92417010eede8285f57562d827eb56a
Georg Wölflein <georgw7777@gmail.com>
2017-11-18 11:01:13 +0000

    Write keyword analysis

--- backEnd/KeywordAnalysis.java
+++ backEnd/KeywordAnalysis.java
@@ -1,6 +1,13 @@
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.NaturalLanguageUnderstanding;
 import com.ibm.watson.developer_cloud.natural_language_understanding.v1.model.*;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class KeywordAnalysis {
 
     public static void main(String[] args){
@@ -10,27 +17,49 @@ public class KeywordAnalysis {
                 "6kdbQDL4cmAg"
         );
 
-//        String url = "Scraper/out/abrikosov-bio.html";
-        String url = "www.cnn.com";
-        KeywordsOptions keywords= new KeywordsOptions.Builder()
-                .sentiment(true)
-                .emotion(true)
-                .limit(3)
-                .build();
 
-        Features features = new Features.Builder()
-                .keywords(keywords)
-                .build();
+        try {
+            FileWriter writer = new FileWriter("output-keyword-analysis.json");
+            File dir = new File("Scraper/out/");
+            File[] directoryListing = dir.listFiles();
+            int i = 0;
+            if (directoryListing != null) {
+                for (File child : directoryListing) {
 
-        AnalyzeOptions parameters = new AnalyzeOptions.Builder()
-                .url(url)
-                .features(features)
-                .build();
+                    try {
+                        String filePath = child.toString();
+                        String url = "gw66.host.cs.st-andrews.ac.uk/" + filePath;
+                        System.out.println(i++ + ": " + url);
+                        KeywordsOptions keywords= new KeywordsOptions.Builder()
+                                .sentiment(true)
+                                .emotion(true)
+                                .limit(10)
+                                .build();
 
-        AnalysisResults response = service
-                .analyze(parameters)
-                .execute();
-        System.out.println(response);
+                        Features features = new Features.Builder()
+                                .keywords(keywords)
+                                .build();
+
+                        AnalyzeOptions parameters = new AnalyzeOptions.Builder()
+                                .url(url)
+                                .features(features)
+                                .build();
+
+                        AnalysisResults response = service
+                                .analyze(parameters)
+                                .execute();
+                        writer.write(String.valueOf(response) + ",\n");
+                        writer.flush();
+
+                    }catch (Exception e){}
+                }
+            } else {
+                return;
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
