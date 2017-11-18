100d92432e2e9c87302535231af378796f3561df
dylanOshima <2017.dylan.oshima@isak.jp>
2017-11-18 08:16:43 +0100

    WAI WON'T THIS WORK? '

--- newFrontEnd/feed.js
+++ newFrontEnd/feed.js
@@ -1,23 +1,43 @@
-//Not Working
-// $(".show-more a").on("click", () => {
-//     var $this = $(this);
-//     var $content = $this.parent().prev("div.content");
-//     var linkText = $this.text().toUpperCase();
-//
-//     if(linkText === "SHOW MORE"){
-//         linkText = "Show less";
-//         $content.switchClass("hideContent", "showContent", 400);
-//     } else {
-//         linkText = "Show more";
-//         $content.switchClass("showContent", "hideContent", 400);
-//     };
-//
-//     $this.text(linkText);
-// });
+$(".show-more a").each(function() {
+    var $link = $(this);
+    var $content = $link.parent().prev("div.text-content");
 
-if (window.File && window.FileReader && window.FileList && window.Blob) {
-  var json = JSON.parse('./practiceInputs/Martin-Luther-King.json');
-  console.log(json);
-} else {
-  alert('The File APIs are not fully supported in this browser.');
+    console.log($link);
+
+    var visibleHeight = $content[0].clientHeight;
+    var actualHide = $content[0].scrollHeight - 1;
+
+    console.log(actualHide);
+    console.log(visibleHeight);
+
+    if (actualHide > visibleHeight) {
+        $link.show();
+    } else {
+        $link.hide();
+    }
+});
+
+$(".show-more a").on("click", function() {
+  var $link = $(this);
+  var $content = $link.parent().prev("div.text-content");
+  var linkText = $link.text();
+
+  $content.toggleClass("short-text, full-text");
+
+  $link.text(getShowLinkText(linkText));
+
+  return false;
+});
+
+function getShowLinkText(currentText) {
+  console.log("getShowLinkText")
+    var newText = '';
+
+    if (currentText.toUpperCase() === "SHOW MORE") {
+        newText = "Show less";
+    } else {
+        newText = "Show more";
+    }
+
+    return newText;
 }
