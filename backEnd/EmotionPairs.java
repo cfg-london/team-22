9e0d90cfbf7338a71e58df1ba9461e25f94b3332
Zenya <jevgenijchom@gmail.com>
2017-11-18 10:13:15 +0000

    Implement keyword API

---
+++ backEnd/EmotionPairs.java
@@ -0,0 +1,15 @@
+/**
+ * Created by jevge on 18/11/2017.
+ */
+public class EmotionPairs {
+
+    String name,pair1,pair2,emotion;
+
+    public EmotionPairs(String name, String pair1, String pair2, String emotion){
+        this.name = name;
+        this. pair1 = pair1;
+        this.pair2 = pair2;
+        this.emotion = emotion;
+    }
+
+}
