c81e1675afea2d427b35ac197da1fd7d3415c936
dylanOshima <2017.dylan.oshima@isak.jp>
2017-11-18 06:33:39 +0100

    #SoManyBugsInTheFront Smaller tweaks

--- newFrontEnd/feed.js
+++ newFrontEnd/feed.js
@@ -1,22 +1,22 @@
 //Not Working
-$(".show-more a").on("click", () => {
-    var $this = $(this);
-    var $content = $this.parent().prev("div.content");
-    var linkText = $this.text().toUpperCase();
-
-    if(linkText === "SHOW MORE"){
-        linkText = "Show less";
-        $content.switchClass("hideContent", "showContent", 400);
-    } else {
-        linkText = "Show more";
-        $content.switchClass("showContent", "hideContent", 400);
-    };
-
-    $this.text(linkText);
-});​
+// $(".show-more a").on("click", () => {
+//     var $this = $(this);
+//     var $content = $this.parent().prev("div.content");
+//     var linkText = $this.text().toUpperCase();
+//
+//     if(linkText === "SHOW MORE"){
+//         linkText = "Show less";
+//         $content.switchClass("hideContent", "showContent", 400);
+//     } else {
+//         linkText = "Show more";
+//         $content.switchClass("showContent", "hideContent", 400);
+//     };
+//
+//     $this.text(linkText);
+// });​
 
 if (window.File && window.FileReader && window.FileList && window.Blob) {
-  var json = JSON.parse(window.FileReader('./practiceInputs/Martin-Luther-King.json'));
+  var json = JSON.parse('./practiceInputs/Martin-Luther-King.json');
   console.log(json);
 } else {
   alert('The File APIs are not fully supported in this browser.');
